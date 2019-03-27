5f66aca1cc552e9e30d4211a3c1acc763dc41998
kaleido <kaleido@gmail.com>
2019-03-28 07:24:20 +0900

    learn class component and state and setState

--- src/index.js
+++ src/index.js
@@ -1,29 +1,44 @@
-//import the react and ReactDom libralies
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from './CommnentDetail';
-import ApprovalCard from './ApprovalCard';
-// create a react component
 
-const App=()=>{
 
-  return (
-   
-  <div className="ui container comments">
-    <ApprovalCard>
-      <CommentDetail author="sam" created_at="am7:00"/>
-    </ApprovalCard>
 
-    <CommentDetail author="tom" created_at="am5:00"/>
-    <CommentDetail author="pop" created_at="am3:00"/>
-  </div>
-  )
+class App extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state={lat:null,error:null};
+    window.navigator.geolocation.getCurrentPosition(
+      (position)=>{
+        this.setState({lat:position.coords.latitude})
+      },
+      (err)=>{
+        this.setState({error:err.message})
+      }
+    
+    )
+  }
+
+
+  render(){
+    if(!this.state.error && !this.state.lat){
+      return <div>loading</div>
+
+    }
+    if(!this.state.error){
+      return <div>latitude:{this.state.lat}</div> 
+    }
+    
+    else{
+      return <div>error:{this.state.error}</div>
+    }
+  }
 }
 
-//Take the react component and show it on the screen
 
 ReactDOM.render(
+
   <App />,
   document.querySelector("#root")
+
 )
