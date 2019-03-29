a2013a1695bf68cd67632d911e388f6c6f99c818
kaleido <kaleido@gmail.com>
2019-03-29 14:22:18 +0900

    refactoring with chain methods

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,9 +7,12 @@ export const fetchPostsAndUsers=()=>async (dispatch,getState)=>{
   //データを取得するまで先へ進まない
   await dispatch(fetchPosts())
 
-  const userIds=_.uniq(_.map(getState().posts,'userId'))
 
-  userIds.forEach(id=>dispatch(fetchUser(id)))
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id=>dispatch(fetchUser(id)))
+    .value()
 
 }
 
